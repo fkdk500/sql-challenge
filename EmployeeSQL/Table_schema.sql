6b0f92e15e11c10b77ac136c9eada160a195e51b
fkdk500 <fkdk500@gmail.com>
2023-03-01 00:17:48 -0600

    Refined version upload

--- EmployeeSQL/Table_schema.sql
+++ EmployeeSQL/Table_schema.sql
@@ -1,16 +1,29 @@
 -- Creating table schema
 
+-- Create table departments
+DROP TABLE departments
+
+CREATE TABLE departments (
+  dept_no VARCHAR(4) NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (dept_no)
+);
+--view the table
+select * from departments
+
+
 --Create titles table
 DROP TABLE titles
 
 CREATE TABLE titles (
-	title_id VARCHAR NOT NULL,
+	title_id VARCHAR(5) NOT NULL,
 	title VARCHAR(20) NOT NULL,
 	PRIMARY KEY(title_id) 
 );
 
 --view the table
 select * from titles
+
 
 -- Create table employees
 DROP TABLE employees
@@ -30,40 +43,30 @@ CREATE TABLE employees (
 --view the table
 select * from employees
 
--- Create table departments
-DROP TABLE departments
-
-CREATE TABLE departments (
-  dept_no VARCHAR(10),
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (dept_no)
-);
-
---view the table
-select * from departments
 
 --Create table dept_emp
 DROP TABLE dept_emp
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(10) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no,dept_no)
 );
-
 --view the table
 select * from dept_emp
 
 
--- Create table dept_managers
-DROP TABLE dept_managers
+-- Create table dept_manager
+DROP TABLE dept_manager
 
-CREATE TABLE dept_managers (
- dept_no VARCHAR (10),
- emp_no INT,
- FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
- FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+CREATE TABLE dept_manager (
+ 	dept_no VARCHAR (4),
+ 	emp_no INT,
+ 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+ 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no,emp_no)
 );
 
 --view the table
@@ -76,7 +79,7 @@ DROP TABLE salaries
 
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
-	salary BIGINT NOT NULL,
+	salary INT NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
