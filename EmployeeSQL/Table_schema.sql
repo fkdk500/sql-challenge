e67644ca4d70a1f7d029bf3478097d453e235dff
fkdk500 <fkdk500@gmail.com>
2023-03-02 16:35:21 -0600

    revised version after office hr discussion

--- EmployeeSQL/Table_schema.sql
+++ EmployeeSQL/Table_schema.sql
@@ -62,16 +62,13 @@ select * from dept_emp
 DROP TABLE dept_manager
 
 CREATE TABLE dept_manager (
- 	dept_no VARCHAR (4),
- 	emp_no INT,
- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (dept_no,emp_no)
+ 	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL PRIMARY KEY	
 );
 
 --view the table
-select * from dept_managers
-
+select * from dept_manager
 
 
 --Creating salaries table
@@ -80,7 +77,8 @@ DROP TABLE salaries
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
 );
 
 --view the table
